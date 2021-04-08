68a89e94f8788d4216566ec3d9093842b8a33243
Shaho Parvin (Interview) <shaho.parvini@gmail.com>
2021-04-08 13:55:47 +0430

    add customer scope ans relation role model to user

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Role extends Model
 {
     use HasFactory;
+
+    public function scopeCustomer($query)
+    {
+        return $query->where('title', 'customer');
+    }
+
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class);
+    }
 }
