3a05f7f5dea2897e1fb7581db69ba4da9f36c07e
shaho <shaho.parvini@gmail.com>
2021-04-12 00:56:11 +0430

    remove role id from user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,14 +29,13 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
-            'role_id' => Role::customer()->first()->id
         ];
     }
 
     /**
      * Indicate that the model's email address should be unverified.
      *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     * @return Factory
      */
     public function unverified()
     {
