b0df3d0f97dd3be038d20548aaf43fca5b7f82ee
shaho <shaho.parvini@gmail.com>
2021-04-12 00:55:31 +0430

    add policy for option model

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Option;
+use App\Policies\OptionPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -14,6 +16,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        Option::class => OptionPolicy::class,
     ];
 
     /**
