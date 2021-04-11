b7aaa2bde404304bc6a5dd019796f6886a041345
shaho <shaho.parvini@gmail.com>
2021-04-12 00:54:06 +0430

    add scope manager and change relationship with user

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,19 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Role extends Model
 {
     use HasFactory;
 
+    public function scopeManager($query)
+    {
+        return $query->where('title', 'manager');
+    }
+
     public function scopeCustomer($query)
     {
         return $query->where('title', 'customer');
     }
 
-    public function users(): HasMany
+    public function users(): BelongsToMany
     {
-        return $this->hasMany(User::class);
+        return $this->belongsToMany(User::class);
     }
 }
