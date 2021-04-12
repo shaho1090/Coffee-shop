84ec0364c5dbe07a56085d5f67b1d3ff1b61334e
shaho <shaho.parvini@gmail.com>
2021-04-12 12:18:18 +0430

    product and product variant migrations

---
+++ database/migrations/2021_04_12_035447_create_product_variants_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductVariantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_variants', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained('products','id');
+            $table->foreignId('option_id')->constrained('options','id');
+            $table->unsignedBigInteger('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_variants');
+    }
+}
