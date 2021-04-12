fcc2f5752ad5ec6889403a006627cc14cd4d794d
shaho <shaho.parvini@gmail.com>
2021-04-12 12:20:28 +0430

    product policy assignment

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\Models\Option;
+use App\Models\Product;
 use App\Policies\OptionPolicy;
+use App\Policies\ProductPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -17,6 +19,7 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
         Option::class => OptionPolicy::class,
+        Product::class => ProductPolicy::class,
     ];
 
     /**
