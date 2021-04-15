8993c38fc85ebf278e07f529a1a919d64dd1890a
Shaho Parvin (Interview) <shaho.parvini@gmail.com>
2021-04-15 14:03:40 +0430

    add destroy method for canceling the order

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Resources\OrderHeaderCollection;
 use App\Http\Resources\OrderHeaderResource;
 use App\Models\OrderHeader;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -37,11 +38,35 @@ class OrdersController extends Controller
         DB::beginTransaction();
 
         if (!$order->isInWaitingState()) {
-            throw new \Exception('The order can not be changed!');
+            return response()->json([
+                'error' => true,
+                'message' => 'The order can not be changed!'
+            ]);
         }
 
         $order->updateLines($request->get('order_data'));
 
         DB::commit();
+
+        $order->refresh();
+
+        return new OrderHeaderResource($order);
+    }
+
+    public function destroy($order): JsonResponse
+    {
+        if (!$order->isInWaitingState()) {
+            return response()->json([
+                'error' => true,
+                'message' => 'The order can not be canceled!'
+            ]);
+        }
+
+        $order->delete();
+
+        return response()->json([
+            'error' => false,
+            'message' => 'The order has been canceled!'
+        ]);
     }
 }
